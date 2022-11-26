ac4a8dbce740143e7b2c1cd82650193a2c3a50e9
Amit Kumar Das <amit.inverse@gmail.com>
2022-11-26 19:01:23 +0530

    Updating States

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log('Need to add book with : ', title);
+        // BAD CODE!
+        books.push({ id: 123, title: title });
+        console.log(books);
+        setBooks(books);
     };
 
     return (
         <div>
+            {books.length}
             <BookCreate onCreate={createBook} />
         </div>
     );
