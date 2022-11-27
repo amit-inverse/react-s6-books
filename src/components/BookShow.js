82c97a3ebc3284988cf662d74af25219c5832d27
Amit Kumar Das <amit.inverse@gmail.com>
2022-11-27 12:28:55 +0530

    Collapsing Two Handlers into One

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -12,13 +12,14 @@ function BookShow({ book, onEdit, onDelete }) {
         setShowEdit(!showEdit);
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = (id, newTitle) => {
         setShowEdit(false);
+        onEdit(id, newTitle);
     };
 
     let content = <h3>{book.title}</h3>;
     if (showEdit) {
-        content = <BookEdit onEdit={onEdit} onSubmit={handleSubmit} book={book} />;
+        content = <BookEdit onSubmit={handleSubmit} book={book} />;
     }
 
     return (
