5416249804d86b869b821919513ef719acb86f8c
Amit Kumar Das <amit.inverse@gmail.com>
2022-11-27 10:46:19 +0530

    Displaying the List

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        const updatedBooks = [...books, { id: 123, titl; }];
+        const updatedBooks = [...books, { id: Math.round(Math.random() * 999), title }];
         setBooks(updatedBooks);
     };
 
     return (
-        <div>
-            {books.length}
+        <div className="app">
+            <BookList books={books} />
             <BookCreate onCreate={createBook} />
         </div>
     );
