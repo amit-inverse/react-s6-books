247fffe3fd729965080beb08f3917bde8af81c35
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-10 18:04:32 +0530

    More on Changing Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,14 +3,15 @@ import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onEdit, onDelete }) {
-    const value = useContext(BooksContext);
+    const { count, incrementCount } = useContext(BooksContext);
 
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
     });
     return (
         <div className="book-list">
-            {value}
+            {count}
+            <button onClick={incrementCount}>Click</button>
             {renderedBooks}
         </div>
     );
